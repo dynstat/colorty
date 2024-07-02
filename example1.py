9ed64a875dfef0c832cb5f67db504e4bac817f9b
dynstat <vs15vivek@gmail.com>
2024-07-03 01:09:22 +0530

    example file modified

--- example1.py
+++ example1.py
@@ -1,4 +1,15 @@
 # Example usage of the library
 from colort import Clt
+from colort import set_color, reset_color
 
-print(f"This is {Clt.RED}red{Clt.RESET} text.")
+
+set_color('31')  # Set text color to red
+set_color('31')  # Set text color to red
+print("This text is red")
+print("This text is still red")
+set_color('32')  # Set text color to green
+print("This text is green")
+reset_color()    # Reset text color to default
+print("This text is default color")
+# format partial string with color
+print(f"This is {Clt.RED}partial red{Clt.RESET} and {Clt.BLUE}partial blue{Clt.RESET} text.")
