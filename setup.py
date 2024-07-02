825c6e828952173ea92f22fb98490956b64672c0
dynstat <vs15vivek@gmail.com>
2024-07-03 01:10:34 +0530

    setup.py info added

--- setup.py
+++ setup.py
@@ -1,21 +1,35 @@
-# Setup script for packaging and distribution
+"""
+Setup script for packaging and distribution of the 'colort' library.
+"""
+
 from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
 
 setup(
     name='colort',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),  # Exclude test packages
     install_requires=[],
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A library for handling colored text in terminal environments.',
-    long_description=open('README.md').read(),
+    author='Vivek',
+    author_email='xsvk@gmx.com',
+    description='A simple library for handling colored text in terminal environments.',
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/colort',
+    url='https://github.com/dynstat/colort',
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+    include_package_data=True,  # Include non-Python files specified in MANIFEST.in
+    entry_points={
+        'console_scripts': [
+            'colort=colort.cli:main',  # If you have a CLI, add this line
+        ],
+    },
 )
