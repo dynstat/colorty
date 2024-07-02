ddc4200a5d7444ff7497807663c90310c812a4d8
dynstat <vs15vivek@gmail.com>
2024-07-03 02:50:15 +0530

    file renamed

---
+++ colorize.py
@@ -0,0 +1,15 @@
+# Example usage of the library
+from colorty import Clt
+from colorty import set_color, reset_color
+
+
+set_color('31')  # Set text color to red
+set_color('31')  # Set text color to red
+print("This text is red")
+print("This text is still red")
+set_color('32')  # Set text color to green
+print("This text is green")
+reset_color()    # Reset text color to default
+print("This text is default color")
+# format partial string with color
+print(f"This is {Clt.RED}partial red{Clt.RESET} and {Clt.BLUE}partial blue{Clt.RESET} text.")
